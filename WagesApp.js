7b7ca04f60a3b50ef315410363c6265d9039e741
brianh47 <124638082+brianh47@users.noreply.github.com>
2023-07-02 00:41:14 -0700

    first commit

---
+++ WagesApp.js
@@ -0,0 +1,8 @@
+var rate = document.getElementById("rop");
+var hrs = document.getElementById("hw");
+var wages = document.getElementById("wages");
+
+function calculateWages(){
+    
+    wages = 
+}
